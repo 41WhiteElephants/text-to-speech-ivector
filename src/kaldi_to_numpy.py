29e74ae7475154b64bbbbc89f394e49026f879b6
41WhiteElephants <wacol72@gmail.com>
2019-02-03 22:26:34 +0100

    add working ark2numpy utility

--- src/kaldi_to_numpy.py
+++ src/kaldi_to_numpy.py
@@ -1,38 +1,22 @@
 """numpy-kaldi i/o interface
 """
-import logging
-import struct
 import numpy as np
 
 
-def ark2dict(arkfile):  # bufferized version
-    """Kaldi archive (ark) to dictionnary of numpy arrays (~npz)
-    ..note: Only standard features files supported, and float (no doubles)
-    """
-    res = {}
-    with open(arkfile) as fin:
-        while True:
-            #TODO: break if empty buffer here
-            fname = ''
-            c = fin.read(1)
-            if c == '':  # EOF (EOFError not raised by read(empty))
-                break
-            while c != ' ':
-                fname += c
-                c = fin.read(1)
-            logging.debug(fname)
-            # end of fname
-            fin.read(1)
-            # data type
-            assert fin.read(4) == 'BFM ', 'type not supported'
-            # nrows type
-            assert struct.unpack('b', fin.read(1))[0] == 4,  'type not supported'
-            nrows = struct.unpack('i', fin.read(4))[0]
-            # ncols type:
-            assert struct.unpack('b', fin.read(1))[0] == 4,  'type not supported'
-            ncols = struct.unpack('i', fin.read(4))[0]
-            # data
-            size = nrows * ncols * 4
-            data = np.fromstring(fin.read(size), dtype=np.float32).reshape((nrows, ncols))
-            res[fname] = data
-    return res
+def ark2dict(arkfile):  
+    	"""Kaldi archive (ark) to dictionnary of numpy arrays (~npz)
+    	"""
+	spk_ivec = {}
+	with open(arkfile) as fin:
+            	lines= fin.readlines()
+		for line in lines:	
+			#delete brackets
+			line = line.replace("[", "").replace("]", "").split()
+			spk_name = line[0]
+			print spk_name
+			num_data = map(float,line[1:])
+			print(num_data)
+            
+	    		ivec = np.array(num_data)
+            		spk_ivec[spk_name] = ivec
+	return spk_ivec
---
+++ src/plot_scores.py
@@ -0,0 +1,15 @@
+import matplotlib
+import sklearn.manifold.TSNE
+from kaldi_to_numpy import ark2dict 
+
+def main():
+	ivec_path = "/macierz/home/s174520/ivector-xvector/ivector/data/feat/ivectors_enroll_mfcc/ivector.1.ark"
+	ivec_dict = ark2dict(ivec_path)
+	print len(ivec_dict)
+	do_tsne()
+	plot()
+		
+
+
+if __name__ == "__main__":
+	main()
